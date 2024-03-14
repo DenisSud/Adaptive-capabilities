b48b76a4c9c8976898ddf5e6e02835e3c6883d1c
DenisSud <sudakov.denis.2007@gmail.com>
2024-03-14 13:01:08 +0300

    ok

--- proccess.py
+++ proccess.py
@@ -1,73 +1,76 @@
 import os
-import csv
 import json
 import numpy as np
+import pandas as pd
 
-data_folder = 'pupil_data/'
-results = []
+with open('results.json', encoding="utf8") as json_file:
+    results = json.load(json_file)
 
-for filename in os.listdir(data_folder):
-    if filename.endswith('.csv'):
-        file_path = data_folder + filename
+# Specify the directory containing the combined CSV file
+directory = 'pupil_data/'
 
-        # Read CSV into DataFrame
-        data = []
-        with open(file_path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                data.append(float(row[0]))
+# Read the combined CSV file into a dataframe
+combined_df = pd.read_csv(os.path.join(directory, 'combined.csv'))
 
-        # print(data)
-        
-        # Ask user for 4 values
-        val1, val3 = input(f'\nPlease enter the values for the file: {file_path}: ').split()
-        val1 = int(val1)
-        val3 = int(val3)
-        val2 = val1 + 50
-        val4 = val3 + 50
-    
-        # Get data array
-        data_array = np.array(data)
-        
-        # Calculate 3 averages
-        avg1 = (np.average(data_array[:val1])).round()
-        avg2 = (np.average(data_array[val2:val3])).round() 
-        avg3 = (np.average(data_array[val4:]).round())
+for column in combined_df.columns:
 
-        d1 = avg2 - avg1
-        if avg1 >= avg3:
-            d2 = avg1 - avg3
-        else:
-            d2 = avg3 - avg1
+    if any(column in d['name'] for d in results):
+        print("уже посчитан(a): " + column)
+        continue
+    # Get the data array from the column
+    data_array = combined_df[column]
 
-        speed1 = (data_array[val2]- data_array[val1])/(val2-val1) 
-        speed2 = (data_array[val3]- data_array[val4])/(val4-val3)
-        
-        # print('Averages:')
-        # print(avg1)
-        # print(avg2)
-        # print(avg3)
+    file_name = column.split('/')[-1]
 
-        # print('Differences:')
-        # print(d1)
-        # print(d2)
+    # Ask user for 4 values
+    val1, val3 = input(f'\nPlease enter the values for the file: {column}: ').split()
+    val1 = int(val1)
+    val3 = int(val3)
+    val2 = val1 + 50
+    val4 = val3 + 50
+    print(f"{val1} {val2} {val3} {val4}")
 
-        # print('Speeds:')
-        # print(speed1)
-        # print(speed2)
+    # print(f'\nThe values for the person: {column}: are:\n\t{val1}\n\t{val2}\n{val3}\n\t{val4}')
 
-        result = {
-            'file_path': file_path,
-            'averages': [avg1, avg2, avg3],
-            'differences': [d1, d2],
-            'speeds': [speed1, speed2]
-        }
+    # Calculate 3 averages
+    end = len(data_array)
+    # TODO find the end of the data array (find when NaN values appear) then 
 
-        results.append(result)
+    print(data_array[:val1])
+    print((data_array[val2:val3]))
+    print(data_array[val4:])
+    avg1 = int((data_array[:val1]).sum()/len(data_array[:val1]))
+    avg2 = int((data_array[val2:val3]).sum()/len(data_array[val2:val3]))
+    avg3 = int((data_array[val4:end]).sum()/len(data_array[val4:]))
 
-# Save results to a JSON file
-output_file = 'results.json'
-with open(output_file, 'w') as json_file:
-    json.dump(results, json_file, indent=4)
+    d1 = avg2 - avg1
+    if avg1 >= avg3:
+        d2 = avg1 - avg3
+    else:
+        d2 = avg3 - avg1
+
+    speed1 = (sum(data_array[val2:val2+5])- sum(data_array[val1:val1+5]))/(val2-val1) 
+    speed2 = (sum(data_array[val3:val3+5])- sum(data_array[val4:val4+5]))/(val4-val3)
+
+    result = {
+        'name': file_name,
+        'averages': [avg1, avg2, avg3],
+        'differences': [d1, d2],
+        'speeds': [speed1, speed2]
+    }
+
+    results.append(result)
+
+    output_file = 'results.json'
+    with open(output_file, 'w', encoding="utf8") as json_file:
+        json.dump(results, json_file, indent=4, ensure_ascii=False)
+
+
+# # Save results to a JSON file
+# output_file = 'results.json'
+# with open(output_file, 'w') as json_file:
+#    json.dump(results, json_file, indent=4)
+
+print(results)
 
 print('Results saved to', output_file)
